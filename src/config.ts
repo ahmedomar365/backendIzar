18ecd630e3e35322fb426a4c340944122b226077
ahmedomarar365@gmail.com <ahmedomarar365@gmail.com>
2023-04-21 09:38:25 +0300

    added postgres online url

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,8 @@ export const cloud_name = process.env.cloud_name;
 export const api_key = process.env.api_key;
 
 export const api_secret = process.env.api_secret;
+
+
+export const DB_PASS = process.env.DB_PASS;
+
+export const DB_URL = process.env.DB_URL;
--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,15 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 import { Product } from "./entity/Product"
+import { DB_PASS, DB_URL } from "./config"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: DB_URL,
     port: 5432,
-    username: "postgres",
-    password: "root",
-    database: "postgres",
+    username: "rtpiscxq",
+    password: DB_PASS,
+    database: "rtpiscxq",
     synchronize: true,
     logging: false,
     entities: [User, Product],
